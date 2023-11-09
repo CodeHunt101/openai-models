d314bb3c1973de25bca212938ba72a3f7de35336
haroldtm <harold.torresmarino@endava.com>
2023-11-09 17:03:39 +1100

    modularise helper functions and added types

--- pages/api/visual-analysis.js
+++ pages/api/visual-analysis.js
@@ -2,6 +2,11 @@ import { Configuration, OpenAIApi } from 'openai';
 import fs from 'fs';
 import path from 'path';
 import formidable from 'formidable';
+import {pollForFile, encodeImage, verifyUploadedFile} from '../../utils/helpers'
+
+// Constants
+const POLL_INTERVAL = 1000; // 1 second
+const POLL_TIMEOUT = 60000;
 
 export const config = {
   api: {
@@ -16,123 +21,81 @@ const openai = new OpenAIApi(configuration);
 
 export default async function visualAnalysis(req, res) {
   if (!configuration.apiKey) {
-    res.status(500).json({
+    return res.status(500).json({
       error: {
         message:
           'OpenAI API key not configured, please follow instructions in README.md',
       },
     });
-    return;
   }
 
-  function pollForFile(filePath, interval, timeout) {
-    const start = Date.now();
-
-    return new Promise((resolve, reject) => {
-      const checkFile = () => {
-        fs.access(filePath, fs.constants.R_OK, (err) => {
-          if (!err) {
-            // File exists and is readable
-            resolve(filePath);
-            return filePath;
-          } else if (Date.now() - start >= timeout) {
-            reject(
-              new Error(
-                `File not found within the specified timeout: ${filePath}`
-              )
-            );
-          } else {
-            // Continue polling
-            setTimeout(checkFile, interval);
-          }
-        });
-      };
-
-      checkFile();
-    });
-  }
-
-  function encodeImage(imagePath) {
-    try {
-      const imageBuffer = fs.readFileSync(imagePath);
-      const base64Image = imageBuffer.toString('base64');
-      return base64Image;
-    } catch (error) {
-      console.error(`Error encoding image: ${error.message}`);
-      return null;
+  const form = formidable({});
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      console.log(err)
+      return
     }
-  }
+    const prompt = fields.prompt[0];
+    console.log({ prompt });
+    // Verify prompt is not empty
+    if (prompt.trim().length === 0) {
+      return res.status(400).json({
+        error: {
+          message: 'Please enter a valid prompt',
+        },
+      });
+    }
+    const originalImagePath = verifyUploadedFile(files, res)
+    let imagePath
+    try {
+      await fs.promises.rename(originalImagePath, 'tempImage.jpeg');
+      imagePath = path.join(process.cwd(), 'tempImage.jpeg');
+      
+      // Poll for the final image
+      const finalImagePath = await pollForFile(imagePath, POLL_INTERVAL, POLL_TIMEOUT);
+      
+      const base64Image = encodeImage(finalImagePath);
 
-  let filePath;
-  if (req.method === 'POST') {
-    const form = formidable({});
-    form.parse(req, async (err, fields, files) => {
-      const prompt = fields.prompt[0];
-      if (prompt.trim().length === 0) {
-        res.status(400).json({
-          error: {
-            message: 'Please enter a valid prompt',
-          },
-        });
-        return;
-      }
-      const uploadedFile = files.file;
-      filePath = uploadedFile[0].filepath;
-      fs.rename(filePath, 'tempImage.jpeg', async (moveErr) => {
-        if (moveErr) {
-          console.error(moveErr);
-          return res
-            .status(500)
-            .json({ error: 'Error moving the uploaded file' });
-        }
-
-        const imagePath = path.join(process.cwd(), 'tempImage.jpeg');
-        const pollInterval = 1000; // 1 second
-        const pollTimeout = 60000; // 60 seconds
-
-        const filepath2 = await pollForFile(
-          imagePath,
-          pollInterval,
-          pollTimeout
-        );
-
-        const base64Image = encodeImage(filepath2);
-        console.log({ prompt });
-        try {
-          const chatCompletion = await openai.createChatCompletion({
-            model: 'gpt-4-vision-preview',
-            messages: [
+      const chatCompletion = await openai.createChatCompletion({
+        model: 'gpt-4-vision-preview',
+        messages: [
+          {
+            role: 'user',
+            content: [
+              { type: 'text', text: prompt },
               {
-                role: 'user',
-                ['content']: [
-                  { type: 'text', text: prompt },
-                  {
-                    type: 'image_url',
-                    image_url: `data:image/jpeg;base64,${base64Image}`,
-                  },
-                ],
+                type: 'image_url',
+                image_url: `data:image/jpeg;base64,${base64Image}`,
               },
             ],
-            max_tokens: 300,
-          });
-          const result = chatCompletion.data.choices[0].message?.content;
-          console.log(result);
-          res.status(200).json({ result });
-        } catch (error) {
-          if (error.response) {
-            console.error(error.response.status, error.response.data);
-            res.status(error.response.status).json(error.response.data);
-          } else {
-            console.error(`Error with OpenAI API request: ${error.message}`);
-            res.status(500).json({
-              error: {
-                message: error.message,
-              },
-            });
-          }
-        }
-        fs.unlinkSync(imagePath); //Remove file
+          },
+        ],
+        max_tokens: 600,
       });
-    });
-  }
+      const result = chatCompletion.data.choices[0].message?.content;
+      console.log(result);
+      
+      res.status(200).json({ result });
+      
+    } catch (error) {
+      if (error.response) {
+        console.error(error.response.status, error.response.data);
+        res.status(error.response.status).json(error.response.data);
+      } else {
+        console.error(error.message);
+        res.status(500).json({
+          error: {
+            message: error.message,
+          },
+        });
+      }
+    } finally {
+      // Remove the temporary image file
+      try {
+        await fs.promises.unlink(imagePath);
+      } catch (unlinkError) {
+        console.error(`Error deleting image: ${unlinkError.message}`);
+      }
+    }
+  });
 }
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,6 +1,60 @@
+import formidable from 'formidable';
+import fs from 'fs';
+import { NextApiResponse } from 'next';
+
 export const generatePrompt = (prompt: string) => {
   console.log({ prompt })
   const capitalizedPrompt =
     prompt[0].toUpperCase() + prompt.slice(1).toLowerCase()
   return capitalizedPrompt
+}
+
+export const pollForFile = (filePath: string, interval: number, timeout: number) => {
+  const start = Date.now();
+
+  return new Promise((resolve, reject) => {
+    const checkFile = () => {
+      fs.access(filePath, fs.constants.R_OK, (err) => {
+        if (!err) {
+          // File exists and is readable
+          resolve(filePath);
+          return filePath;
+        } else if (Date.now() - start >= timeout) {
+          reject(
+            new Error(
+              `File not found within the specified timeout: ${filePath}`
+            )
+          );
+        } else {
+          // Continue polling
+          setTimeout(checkFile, interval);
+        }
+      });
+    };
+
+    checkFile();
+  });
+};
+
+export const encodeImage = (imagePath: string) => {
+  try {
+    const imageBuffer = fs.readFileSync(imagePath);
+    const base64Image = imageBuffer.toString('base64');
+    return base64Image;
+  } catch (error: any) {
+    console.error(`Error encoding image: ${error.message}`);
+    return null;
+  }
+};
+
+export const verifyUploadedFile = (files: formidable.Files<string>, res: NextApiResponse) => {
+  const uploadedFile = files.file;
+    if (!uploadedFile || !uploadedFile[0] || uploadedFile[0].size === 0 || uploadedFile[0].size > 2e+7) {
+      return res.status(400).json({
+        error: {
+          message: 'Please upload a valid file',
+        },
+      });
+    }
+  return uploadedFile[0].filepath
 }
