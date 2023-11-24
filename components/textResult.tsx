b0353691588b2498cc177e019d4782e094a69c1d
haroldtm <harold.torresmarino@endava.com>
2023-11-24 21:48:05 +1100

    add clipboard copier button

--- components/textResult.tsx
+++ components/textResult.tsx
@@ -1,13 +1,33 @@
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import remarkBreaks from 'remark-breaks'
-import { Message } from '@/types/types'
+import React, { useRef, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import remarkBreaks from 'remark-breaks';
+import { Message } from '@/types/types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboard } from '@fortawesome/free-solid-svg-icons';
 
 type TextResultProps = {
-  messages: Message[] | string
-}
+  messages: Message[] | string;
+};
 
 const TextResult = ({ messages }: TextResultProps) => {
+  const assistantRef = useRef<HTMLDivElement>(null);
+  const [tooltipMessage, setTooltipMessage] = useState<string | null>(null);
+
+  const copyToClipboard = () => {
+    if (assistantRef.current) {
+      const textToCopy = assistantRef.current.innerText;
+      navigator.clipboard
+        .writeText(textToCopy)
+        .catch((err) => {
+          console.error('Unable to copy text to clipboard', err);
+        });
+    }
+
+    setTooltipMessage('Copied to clipboard');
+    setTimeout(() => setTooltipMessage(null), 2000);
+  };
+
   if (Array.isArray(messages)) {
     return (
       <>
@@ -17,33 +37,48 @@ const TextResult = ({ messages }: TextResultProps) => {
             className="card my-2 w-full shadow-lg self-start bg-primary"
           >
             <div className="card-body">
-              {
-                <div className="max-w-xl my-1">
-                  <kbd className="kbd kbd-md">You</kbd>
-                  <p className="my-1 ml-1 text-primary-content">{user}</p>
+              <div className="max-w-xl my-1">
+                <kbd className="kbd kbd-md">You</kbd>
+                <p id="user-prompt" className="my-1 ml-1 text-primary-content">
+                  {user}
+                </p>
+              </div>
+              <div className="max-w-xl my-1">
+                <kbd className="kbd kbd-md">Assistant</kbd>
+                <div
+                  id="asistant-response"
+                  className="prose prose-neutral ml-1"
+                  ref={assistantRef}
+                >
+                  <ReactMarkdown
+                    className="text-primary-content"
+                    remarkPlugins={[remarkGfm, remarkBreaks]}
+                  >
+                    {assistant}
+                  </ReactMarkdown>
                 </div>
-              }
-              {
-                <div className="max-w-xl my-1">
-                  <kbd className="kbd kbd-md">Assistant</kbd>
-                  <div className="prose prose-neutral ml-1">
-                    <ReactMarkdown
-                      className="text-primary-content"
-                      remarkPlugins={[remarkGfm, remarkBreaks]}
+                <div className="flex flex-row-reverse">
+                  <div className="tooltip" data-tip={tooltipMessage}>
+                    <button
+                      onClick={copyToClipboard}
+                      className="btn btn-square btn-outline border-primary bg-neutral-content hover:bg-neutral-content hover:border-neutral-content"
                     >
-                      {assistant}
-                    </ReactMarkdown>
+                      <FontAwesomeIcon
+                        className="text-primary-content"
+                        icon={faClipboard}
+                      />
+                    </button>
                   </div>
                 </div>
-              }
+              </div>
             </div>
           </div>
         ))}
       </>
-    )
+    );
   }
 
-  return <p>{messages}</p>
-}
+  return <p>{messages}</p>;
+};
 
-export default TextResult
+export default TextResult;
